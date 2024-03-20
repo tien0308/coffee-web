4fcecfdef5a18043693392416d74ca75e5159f20
tiendev <vutien0308@gmail.com>
2024-03-20 19:10:34 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+ const $ = document.querySelector.bind(document);
+ const $$ = document.querySelectorAll.bind(document);
+
+ const buyBtns = document.querySelectorAll('.js-buy-cart-one, .js-buy-cart-two, .js-buy-cart-three');
+
+ 
